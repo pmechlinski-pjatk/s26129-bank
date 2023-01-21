90918a90580c6ae7b83eae56e0e6d75387859ee2
Paweł Mechliński <s26129@pjwstk.edu.pl>
2023-01-21 14:58:18 +0100

    Spring Initilizr

---
+++ bank/bank/src/main/java/com/s26129/bank/bank/Application.java
@@ -0,0 +1,13 @@
+package com.s26129.bank.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ bank/bank/src/test/java/com/s26129/bank/bank/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s26129.bank.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
