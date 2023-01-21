e165d74e964844c272678e67086c3d593bd039ec
Paweł Mechliński <s26129@pjwstk.edu.pl>
2023-01-21 16:48:33 +0100

    Working mock test

--- bank/bank/src/test/java/com/s26129/bank/bank/BankServiceUnitTests.java
+++ bank/bank/src/test/java/com/s26129/bank/bank/BankServiceUnitTests.java
@@ -12,9 +12,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -31,72 +33,75 @@ class bankServiceUnitTests {
     private BankService bankService;
 
     @Test
-    void newClientCanBeregistered(){
-        when(clientStorage.getClientList()).add(any())
+    void newClientCanBeRegistered(){
+
+        when(clientStorage.getClientList()).thenReturn(new ArrayList<Client>());
+        int returnCode = bankService.registryNewClient(555);
+        assertThat(returnCode).isEqualTo(0);
     }
-
-    public int registryNewClient(double entryBalance) {
-        if (entryBalance >= 0) {
-            clientStorage.getClientList().add(new Client(clientStorage.getLastClientId() + 1, entryBalance));
-            return 0;
-        } else {
-            return 1;
-        }
-    }
-
-    public TransactionStatus createNewTransaction(int senderId, double transactionValue) {
-        Client client = clientStorage.findClientById(senderId);
-        if (client == null) {
-            return new TransactionStatus("Sender doesn't exist!");
-        }
-
-        double clientBalance = clientStorage.findClientById(senderId).getBalance();
-        if (clientBalance < 0) {
-            return new TransactionStatus("Transaction value exceeds sender's balance!");
-        }
-
-        TransactionStatus transactionStatus = new TransactionStatus(clientBalance, transactionValue);
-        client.setBalance(transactionStatus.getFinalBalance());
-
-        transactionStorage.getTransactionList().add(new Transaction( transactionStorage.getTransactionList().size()+1, transactionValue, client));
-        return new TransactionStatus(clientBalance, transactionValue);
-    }
-
-    public TransactionStatus addToBalance(int senderId, double transactionValue) {
-        if (transactionValue <= 0) {
-            return new TransactionStatus("Tried to order transaction with a negative value!");
-        }
-        try {
-            clientStorage.findClientById(senderId);
-        } catch (NullPointerException e) {
-            return new TransactionStatus("Sender doesn't exist!");
-        }
-        Client client = clientStorage.findClientById(senderId);
-        double clientBalance = clientStorage.findClientById(senderId).getBalance();
-        TransactionStatus transactionStatus = new TransactionStatus(clientBalance, transactionValue);
-        client.setBalance(transactionStatus.getFinalBalance());
-        return transactionStatus;
-    }
-
-    public Map<String, String> getClientData(int clientId) {
-        Map<String, String> outputData = new HashMap<>();
-        try {
-            clientStorage.findClientById(clientId);
-        } catch (NullPointerException e) {
-            return outputData;
-        }
-        Client client = clientStorage.findClientById(clientId);
-        outputData.put("ID", String.valueOf(client.getClientId()));
-        outputData.put("Balance", String.valueOf(client.getBalance()));
-
-        int personalTransactionNumber = 1;
-        for (Transaction transaction : transactionStorage.getTransactionList()) {
-            if (transaction.getTransactionId() == clientId) {
-                outputData.put("Transaction no." + personalTransactionNumber, transaction.toString());
-                personalTransactionNumber++;
-            }
-
-        }
-        return outputData;
-    }
+//
+//    public int registryNewClient(double entryBalance) {
+//        if (entryBalance >= 0) {
+//            clientStorage.getClientList().add(new Client(clientStorage.getLastClientId() + 1, entryBalance));
+//            return 0;
+//        } else {
+//            return 1;
+//        }
+//    }
+//
+//    public TransactionStatus createNewTransaction(int senderId, double transactionValue) {
+//        Client client = clientStorage.findClientById(senderId);
+//        if (client == null) {
+//            return new TransactionStatus("Sender doesn't exist!");
+//        }
+//
+//        double clientBalance = clientStorage.findClientById(senderId).getBalance();
+//        if (clientBalance < 0) {
+//            return new TransactionStatus("Transaction value exceeds sender's balance!");
+//        }
+//
+//        TransactionStatus transactionStatus = new TransactionStatus(clientBalance, transactionValue);
+//        client.setBalance(transactionStatus.getFinalBalance());
+//
+//        transactionStorage.getTransactionList().add(new Transaction( transactionStorage.getTransactionList().size()+1, transactionValue, client));
+//        return new TransactionStatus(clientBalance, transactionValue);
+//    }
+//
+//    public TransactionStatus addToBalance(int senderId, double transactionValue) {
+//        if (transactionValue <= 0) {
+//            return new TransactionStatus("Tried to order transaction with a negative value!");
+//        }
+//        try {
+//            clientStorage.findClientById(senderId);
+//        } catch (NullPointerException e) {
+//            return new TransactionStatus("Sender doesn't exist!");
+//        }
+//        Client client = clientStorage.findClientById(senderId);
+//        double clientBalance = clientStorage.findClientById(senderId).getBalance();
+//        TransactionStatus transactionStatus = new TransactionStatus(clientBalance, transactionValue);
+//        client.setBalance(transactionStatus.getFinalBalance());
+//        return transactionStatus;
+//    }
+//
+//    public Map<String, String> getClientData(int clientId) {
+//        Map<String, String> outputData = new HashMap<>();
+//        try {
+//            clientStorage.findClientById(clientId);
+//        } catch (NullPointerException e) {
+//            return outputData;
+//        }
+//        Client client = clientStorage.findClientById(clientId);
+//        outputData.put("ID", String.valueOf(client.getClientId()));
+//        outputData.put("Balance", String.valueOf(client.getBalance()));
+//
+//        int personalTransactionNumber = 1;
+//        for (Transaction transaction : transactionStorage.getTransactionList()) {
+//            if (transaction.getTransactionId() == clientId) {
+//                outputData.put("Transaction no." + personalTransactionNumber, transaction.toString());
+//                personalTransactionNumber++;
+//            }
+//
+//        }
+//        return outputData;
+//    }
 }
